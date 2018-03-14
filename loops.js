3eb1e0a6214f141d8c5c9ebc5ce3e11289b9364e
tarheelnc83@outlook.com <tarheelnc83@outlook.com>
2018-03-14 19:25:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,11 +16,14 @@ function whileLoop(num){
   return "done"
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
 
-function doWhileLoop(num){
-  while (num>0) {
-    console.log(num);
-    num--
-  }
-  return "done"
+function doWhileLoop(array){
+  do{
+      array.push("I am ${i} strange loops.")
+
+  } while (array.length > 0 && maybeTrue());
+  return array
 }
